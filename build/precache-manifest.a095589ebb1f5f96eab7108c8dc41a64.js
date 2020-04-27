42930ed00c882ef4a34cbd49297e9fd1deeec136
El-Mavewrick <60680552+El-Mavewrick@users.noreply.github.com>
2020-04-27 04:34:29 +0100

    initial upload

---
+++ build/precache-manifest.a095589ebb1f5f96eab7108c8dc41a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4af84c8555952871d87bf8200b6e4d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d65bf719f37d21f0d2d",
+    "url": "/static/css/main.659bff9a.chunk.css"
+  },
+  {
+    "revision": "162b4003122e872ea7ba",
+    "url": "/static/js/2.f0c7e378.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f0c7e378.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d65bf719f37d21f0d2d",
+    "url": "/static/js/main.171339e6.chunk.js"
+  },
+  {
+    "revision": "9196175769b6570a9dea",
+    "url": "/static/js/runtime-main.f88cfdd6.js"
+  }
+]);
